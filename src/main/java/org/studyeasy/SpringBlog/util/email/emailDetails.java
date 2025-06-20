db1ba4e17f83713d599d9fcc5dca3130c6a921d1
viru0909-dev <gadekarvirendra4@gmail.com>
2025-06-20 23:08:58 +0530

    UI Upadates

--- src/main/java/org/studyeasy/SpringBlog/util/email/emailDetails.java
+++ src/main/java/org/studyeasy/SpringBlog/util/email/emailDetails.java
@@ -11,4 +11,29 @@ public class EmailDetails {
         private String recipient;
         private String msgBody;
         private String subject;
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getMsgBody() {
+        return msgBody;
+    }
+
+    public void setMsgBody(String msgBody) {
+        this.msgBody = msgBody;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
 }
